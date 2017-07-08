56d8ae4c08473d84891064f638b558b638af2cc9
Raghunandan Balachandran <raghu@Raghunandans-MacBook-Air.local>
2017-07-08 20:00:11 +0530

    Added vendor JS management.

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ var path = require('path');
 
  module.exports = {
      entry: {
+      index: './js/index.js',
       app: './js/app.js',
       test: './js/test.js',
       style: './css/style.css',
-      test: './css/test.css'
+      test: './css/test.css',
+      vendor: ['lodash', 'jquery']
      },
      output: {
          path: path.resolve(__dirname, 'build'),
@@ -21,13 +23,21 @@ var path = require('path');
                      presets: ['es2015']
                  }
              },
-             { test: /\.css$/,
+             {
+               test: /\.css$/,
                loader: "style-loader!css-loader"
              },
+             {
+               test: /\.(png|svg|jpg|gif)$/, 
+               loader: "file-loader"
+             }
          ]
      },
      stats: {
          colors: true
      },
-     devtool: 'source-map'
+     devtool: 'source-map',
+     plugins: [
+       new webpack.optimize.UglifyJsPlugin()
+     ]
  };
